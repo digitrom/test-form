4d9d4fff320dd6051c0c5caf91ae2324da574ee0
digitrom <roma.bobrovski@gmail.com>
2024-07-01 23:08:19 +0200

    first commit

---
+++ js-1.js
@@ -0,0 +1,8 @@
+let result = +prompt('Введите число','')
+if (result === 0) {
+    alert(0)
+} else if (result < 0) {
+    alert(-1)
+} else {
+    alert(1)
+}
---
+++ js.js
@@ -0,0 +1,6 @@
+let result = prompt('Какое "официальное" название JavaScript?', '');
+if (result === 'ECMAScript') {
+    alert('Верно!');
+} else {
+    alert("Не знаете? ECMAScript!");
+}
